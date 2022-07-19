dffa3977158dde5a50050439ef0bf6de1a7ce1cf
ByeongJuP <pbju92@gmail.com>
2022-07-19 14:04:04 +0900

    add: custom widget

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ A new Flutter project.
 
 This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+# Custom Widget
+## StatelessWidget
+- 생성된 class의 `return`에 원하는 형식값 넣어준다.
+- 재사용이 많은 UI들을 커스텀화 하는게 좋음.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## ListView
+- 스크롤바 자동생성
+- 스크롤 위치 감시 가능
+- 메모리 절약 가능
--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,94 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-            title: const Text('App')
-        ),
-        body: Container(
-          height: 160,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children:[
-              Image.asset('dog.jpeg', width: 150),
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Hi', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50), ),
-                    Text('my name is doge.\nNice to meet you'),
-                    Text('2022.07.15', style: TextStyle(color: Colors.black45)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite, color: Colors.deepOrange,),
-                        Text('4', style: TextStyle(color: Colors.black45))
-                      ],
-                    )
-                  ],
-                ), flex: 1
-              )
-            ]
-          )
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'), Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo'),
+            Text('Hi'),
+            Text('Hello'),
+            Text('Nice to meet you'),
+            Text('woo')
+          ],
         )
       )
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: Text('Hello')
     );
   }
 }
